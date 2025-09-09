217d888c3dd6259ebf83e1bbf2824cc34035d56c
Guilherme Canella <guycanella@gmail.com>
2025-09-09 10:46:27 -0300

    create handlers for services

---
+++ internal/handlers/url_handler.go
@@ -0,0 +1,68 @@
+package handlers
+
+import (
+	"guycanella-url-shortner/internal/models"
+	"guycanella-url-shortner/internal/services"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type URLHandler struct {
+	service services.URLService
+}
+
+func NewURLHandler(service services.URLService) *URLHandler {
+	return &URLHandler{service: service}
+}
+
+func (handler *URLHandler) CreateURL(ctx *gin.Context) {
+	var req models.CreateURLRequest
+
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "invalid request: " + err.Error()})
+		return
+	}
+
+	resp, err := handler.service.CreateURL(req)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "failed to create URL: " + err.Error()})
+	}
+
+	ctx.JSON(http.StatusCreated, resp)
+}
+
+func (handler *URLHandler) Redirect(ctx *gin.Context) {
+	shortCode := ctx.Param("shortCode")
+
+	url, err := handler.service.GetOriginalURL(shortCode)
+	if err != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{"error": "URL not found or expired"})
+		return
+	}
+
+	ctx.Redirect(http.StatusFound, url.OriginalUrl)
+}
+
+func (handler *URLHandler) Stats(ctx *gin.Context) {
+	shortCode := ctx.Param("shortCode")
+
+	stats, err := handler.service.GetURLStats(shortCode)
+	if err != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{"error": "URL not found"})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, stats)
+}
+
+func (handler *URLHandler) Delete(ctx *gin.Context) {
+	id := ctx.Param("id")
+
+	if err := handler.service.DeleteURL(id); err != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{"error": "URL not found"})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"message": "URL deleted successfully"})
+}
